1e74e453231d7e5a6a7dce26f42eb9801fcf0ee5
VeguiDev <veguigameplays@gmail.com>
2026-06-11 21:06:46 -0300

    add Event and EventLocation classes with properties and methods

---
+++ src/main/java/dev/vegui/eventdeck/model/Event.java
@@ -0,0 +1,72 @@
+package dev.vegui.eventdeck.model;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Event {
+
+    private UUID id;
+    private String title;
+    private String description;
+    private LocalDateTime startDate;
+    private Duration duration;
+    private EventLocation location;
+
+    public Event(String title, String description, LocalDateTime startDate, Duration duration, EventLocation location) {
+        this.id = UUID.randomUUID();
+        this.title = title;
+        this.description = description;
+        this.startDate = startDate;
+        this.duration = duration;
+        this.location = location;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Duration duration) {
+        this.duration = duration;
+    }
+
+    public EventLocation getLocation() {
+        return location;
+    }
+
+    public void setLocation(EventLocation location) {
+        this.location = location;
+    }
+}
---
+++ src/main/java/dev/vegui/eventdeck/model/EventLocation.java
@@ -0,0 +1,82 @@
+package dev.vegui.eventdeck.model;
+
+public class EventLocation {
+    private String venueName;
+    private String street;
+    private String city;
+    private String province;
+    private String country;
+
+    public EventLocation(
+            String venueName,
+            String street,
+            String city,
+            String province,
+            String country
+    ) {
+        this.venueName = venueName;
+        this.street = street;
+        this.city = city;
+        this.province = province;
+        this.country = country;
+    }
+
+    public String getFormattedAddress() {
+        return "%s, %s, %s, %s".formatted(
+                street,
+                city,
+                province,
+                country
+        );
+    }
+
+    public String getVenueName() {
+        return venueName;
+    }
+
+    public void setVenueName(String venueName) {
+        this.venueName = venueName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        if (venueName == null || venueName.isBlank()) {
+            return getFormattedAddress();
+        }
+
+        return venueName + " - " + getFormattedAddress();
+    }
+
+}
