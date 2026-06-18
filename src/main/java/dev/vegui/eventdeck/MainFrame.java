ad73160d73cf7d204a94b2c8b3d2231f43b16bbe
VeguiDev <veguigameplays@gmail.com>
2026-06-18 06:50:57 -0300

    add: set application icon in MainFrame constructor

--- src/main/java/dev/vegui/eventdeck/MainFrame.java
+++ src/main/java/dev/vegui/eventdeck/MainFrame.java
@@ -8,6 +8,9 @@ public class MainFrame extends JFrame {
 
     public MainFrame(Router router) {
         this.router = router;
+        ImageIcon icon = new ImageIcon(getClass().getResource("/app.png"));
+        setIconImage(icon.getImage());
+
         setTitle("EventDeck");
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
